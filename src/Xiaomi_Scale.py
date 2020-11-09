0318057ee2a0cad29919032ee766a3514228119d
mpzaborski <mpzaborski@gmail.com>
2020-11-09 20:09:24 +0100

    Fixed issues

--- src/Xiaomi_Scale.py
+++ src/Xiaomi_Scale.py
@@ -11,14 +11,14 @@ from .Xiaomi_Scale_Body_Metrics import BodyMetrics
 
 USER1_GT = int(os.getenv('USER1_GT', '70'))  # If the weight is greater than this number, we're weighing User #1
 USER1_SEX = os.getenv('USER1_SEX', 'male')
-USER1_NAME = os.getenv('USER1_NAME', 'David')
-USER1_HEIGHT = int(os.getenv('USER1_HEIGHT', '175'))  # Height (in cm)
+USER1_NAME = os.getenv('USER1_NAME', 'Mateusz')
+USER1_HEIGHT = int(os.getenv('USER1_HEIGHT', '177'))  # Height (in cm)
 USER1_DOB = os.getenv('USER1_DOB', '1988-09-30')  # DOB (in yyyy-mm-dd format)
 
-USER2_LT = int(os.getenv('USER2_LT', '55'))  # If the weight is greater than this number, we're weighing User #2
+USER2_LT = int(os.getenv('USER2_LT', '65'))  # If the weight is greater than this number, we're weighing User #2
 USER2_SEX = os.getenv('USER2_SEX', 'female')
-USER2_NAME = os.getenv('USER2_NAME', 'Joanne')
-USER2_HEIGHT = int(os.getenv('USER2_HEIGHT', '155'))  # Height (in cm)
+USER2_NAME = os.getenv('USER2_NAME', 'Ania')
+USER2_HEIGHT = int(os.getenv('USER2_HEIGHT', '170'))  # Height (in cm)
 USER2_DOB = os.getenv('USER2_DOB', '1988-10-20')  # DOB (in yyyy-mm-dd format)
 
 
@@ -26,18 +26,20 @@ class ScanProcessor:
 	def __init__(self):
 		self.connected = False
 
-	def getAge(self, d1):
-		d1 = datetime.strptime(d1, "%Y-%m-%d")
-		d2 = datetime.strptime(datetime.today().strftime('%Y-%m-%d'),'%Y-%m-%d')
-		return abs((d2 - d1).days)/365
-
 	def handleDiscovery(self, dev, isNewDev, isNewData):
 		pass
+
+
+def getAge(d1):
+    d1 = datetime.strptime(d1, "%Y-%m-%d")
+    d2 = datetime.strptime(datetime.today().strftime('%Y-%m-%d'),'%Y-%m-%d')
+    return abs((d2 - d1).days)/365
 
 
 def get_scale_data(sdid, desc, data):
 	### Xiaomi V2 Scale ###
 	if data.startswith('1b18') and sdid == 22:
+print('miscale')
 		measunit = data[4:6]
 		measured = int((data[28:30] + data[26:28]), 16) * 0.01
 		unit = ''
@@ -54,16 +56,16 @@ def get_scale_data(sdid, desc, data):
 			print("Scale is sleeping.")
 
 
-def _publish(self, weight, unit, mitdatetime, miimpedance):
+def _publish(weight, unit, mitdatetime, miimpedance):
 	if int(weight) > USER1_GT:
 		user = USER1_NAME
 		height = USER1_HEIGHT
-		age = self.getAge(USER1_DOB)
+		age = getAge(USER1_DOB)
 		sex = USER1_SEX
 	elif int(weight) < USER2_LT:
 		user = USER2_NAME
 		height = USER2_HEIGHT
-		age = self.getAge(USER2_DOB)
+		age = getAge(USER2_DOB)
 		sex = USER2_SEX
 	lib = BodyMetrics(weight, height, age, sex, 0)
 	message = '{'
